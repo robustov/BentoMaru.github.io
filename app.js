f7895e026a2b835584b10653457c5b10d4e14b98
robustov <63014055+robustov@users.noreply.github.com>
2024-01-23 22:09:43 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -182,7 +182,7 @@ btn8m.addEventListener("click", function(){
 tg.MainButton.show()
 
 Telegram.WebApp.onEvent('mainButtonClicked', function(){
-	tg.sendData(am1); 
+	tg.sendData("asdasd"); 
 	//при клике на основную кнопку отправляем данные в строковом виде
 });
 
