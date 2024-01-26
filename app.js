c91e58884b7666223734deef6fb0f0e2d4f0c9aa
robustov <63014055+robustov@users.noreply.github.com>
2024-01-26 17:09:25 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -26,53 +26,108 @@ let btn8p = document.getElementById("btn8+");
 let btn8m = document.getElementById("btn8-");
 let locationb = document.getElementById("loc");
 
-let im1 = document.getElementById("im1");
+//let im1 = document.getElementById("im1");
+
+
+
+let am1 = localStorage.getItem("am1");
+let am2 = localStorage.getItem("am2");
+let am3 = localStorage.getItem("am3");
+let am4 = localStorage.getItem("am4");
+let am5 = localStorage.getItem("am5");
+let am6 = localStorage.getItem("am6");
+let am7 = localStorage.getItem("am7");
+let am8	= localStorage.getItem("am8");
+
+if(am1 == null){
+	document.getElementById("count1").innerText = 0;
+}
+else{
+	document.getElementById("count1").innerText = localStorage.getItem("am1")
+}
+if(am2 == null){
+	document.getElementById("count2").innerText = 0;
+}
+else{
+	document.getElementById("count2").innerText = localStorage.getItem("am2")
+}
+if(am3 == null){
+	document.getElementById("count3").innerText = 0;
+}
+else{
+	document.getElementById("count3").innerText = localStorage.getItem("am3")
+}
+
+if(am4 == null){
+	document.getElementById("count4").innerText = 0;
+}
+else{
+	document.getElementById("count4").innerText = localStorage.getItem("am4")
+}
+
+if(am5 == null){
+	document.getElementById("count5").innerText = 0;
+}
+else{
+	document.getElementById("count5").innerText = localStorage.getItem("am5")
+}
+
+if(am6 == null){
+	document.getElementById("count6").innerText = 0;
+}
+else{
+	document.getElementById("count6").innerText = localStorage.getItem("am6")
+}
+
+if(am7 == null){
+	document.getElementById("count7").innerText = 0;
+}
+else{
+	document.getElementById("count7").innerText = localStorage.getItem("am7")
+}
+
+if(am8 == null){
+	document.getElementById("count8").innerText = 0;
+}
+
+else{
+	document.getElementById("count8").innerText = localStorage.getItem("am8")
+}
+
+//document.getElementById("count2").innerText = am2;
+//document.getElementById("count3").innerText = am3;
+//document.getElementById("count4").innerText = am4;
+//document.getElementById("count5").innerText = am5;
+//document.getElementById("count6").innerText = am6;
+//document.getElementById("count7").innerText = am7;
+//document.getElementById("count8").innerText = am8;
 
 
 
 
-let am1 = 0;
-let am2 = 0;
-let am3 = 0;
-let am4 = 0;
-let am5 = 0;
-let am6 = 0;
-let am7 = 0;
-let am8	 = 0;
-
-
-
-document.getElementById("count1").innerText = am1
-document.getElementById("count2").innerText = am2
-document.getElementById("count3").innerText = am3
-document.getElementById("count4").innerText = am4
-document.getElementById("count5").innerText = am5
-document.getElementById("count6").innerText = am6
-document.getElementById("count7").innerText = am7
-document.getElementById("count8").innerText = am8
 
 
 
 tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
 
 
-im1.addEventListener("click", function(){
-	localStorage.setItem("count1",JSON.stringify(am1));
-});
-
 
 
 btn1p.addEventListener("click", function(){
+		am1 = Number(localStorage.getItem("am1"));
 		am1 = am1+1;
 		document.getElementById("count1").innerText = am1;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am1', am1);
 });
 
 btn1m.addEventListener("click", function(){
+	am1 = Number(localStorage.getItem("am1"));
 	if (am1 > 0) {
 		am1 = am1-1;
 		document.getElementById("count1").innerText = am1;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am1', am1);
 	}
 });
 
@@ -81,16 +136,20 @@ btn1m.addEventListener("click", function(){
 
 
 btn2p.addEventListener("click", function(){
+		am2 = Number(localStorage.getItem("am2"));
 		am2 = am2+1;
 		document.getElementById("count2").innerText = am2;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am2', am2);
 });
 
 btn2m.addEventListener("click", function(){
+	am2 = Number(localStorage.getItem("am2"));
 	if (am2 > 0) {
 		am2 = am2-1;
 		document.getElementById("count2").innerText = am2;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am2', am2);
 	}
 });
 
@@ -99,16 +158,20 @@ btn2m.addEventListener("click", function(){
 
 
 btn3p.addEventListener("click", function(){
+		am3 = Number(localStorage.getItem("am3"));
 		am3 = am3+1;
 		document.getElementById("count3").innerText = am3;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am3', am3);
 });
 
 btn3m.addEventListener("click", function(){
+	am3 = Number(localStorage.getItem("am3"));
 	if (am3 > 0) {
 		am3 = am3-1;
 		document.getElementById("count3").innerText = am3;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am3', am3);
 	}
 });
 
@@ -116,16 +179,20 @@ btn3m.addEventListener("click", function(){
 
 
 btn4p.addEventListener("click", function(){
+		am4 = Number(localStorage.getItem("am4"));
 		am4 = am4+1;
 		document.getElementById("count4").innerText = am4;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am4', am4);
 });
 
 btn4m.addEventListener("click", function(){
+	am4 = Number(localStorage.getItem("am4"));
 	if (am4 > 0) {
 		am4 = am4-1;
 		document.getElementById("count4").innerText = am4;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am4', am4);
 	}
 });
 
@@ -134,16 +201,20 @@ btn4m.addEventListener("click", function(){
 
 
 btn5p.addEventListener("click", function(){
+		am5 = Number(localStorage.getItem("am5"));
 		am5 = am5+1;
 		document.getElementById("count5").innerText = am5;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am5', am5);
 });
 
 btn5m.addEventListener("click", function(){
+	am5 = Number(localStorage.getItem("am5"));
 	if (am5 > 0) {
 		am5 = am5-1;
 		document.getElementById("count5").innerText = am5;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am5', am5);
 	}
 });
 
@@ -152,16 +223,20 @@ btn5m.addEventListener("click", function(){
 
 
 btn6p.addEventListener("click", function(){
+		am6 = Number(localStorage.getItem("am6"));
 		am6 = am6+1;
 		document.getElementById("count6").innerText = am6;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am6', am6);
 });
 
 btn6m.addEventListener("click", function(){
+	am6 = Number(localStorage.getItem("am6"));
 	if (am6 > 0) {
 		am6 = am6-1;
 		document.getElementById("count6").innerText = am6;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am6', am6);
 	}
 });
 
@@ -171,32 +246,40 @@ btn6m.addEventListener("click", function(){
 
 
 btn7p.addEventListener("click", function(){
+		am7 = Number(localStorage.getItem("am7"));
 		am7 = am7+1;
 		document.getElementById("count7").innerText = am7;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am7', am7);
 });
 
 btn7m.addEventListener("click", function(){
+	am7 = Number(localStorage.getItem("am7"));
 	if (am7 > 0) {
 		am7 = am7-1;
 		document.getElementById("count7").innerText = am7;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am7', am7);
 	}
 });
 
 
 
 btn8p.addEventListener("click", function(){
+		am8 = Number(localStorage.getItem("am8"));
 		am8 = am8+1;
 		document.getElementById("count8").innerText = am8;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am8', am8);
 });
 
 btn8m.addEventListener("click", function(){
+	am8 = Number(localStorage.getItem("am8"));
 	if (am8 > 0) {
 		am8 = am8-1;
 		document.getElementById("count8").innerText = am8;
 		tg.MainButton.setText(""+(am1*450+am2*450+am3*800+am4*800+am5*850+am6*800+am7*1250+am8*1250)+"р");
+		localStorage.setItem('am8', am8);
 	}
 });
 
