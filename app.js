b06c3b10f47a18e52ef2fd17e3de1edda47008eb
robustov <63014055+robustov@users.noreply.github.com>
2024-01-31 17:14:40 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 tg.MainButton.show()
 tg.MainButton.textColor = '#FFFFFF';
-tg.MainButton.color = "#FF0000";
-//tg.MainButton.color = '#2cab37';
+tg.MainButton.color = '#2cab37';
 
 
 let item = "";
